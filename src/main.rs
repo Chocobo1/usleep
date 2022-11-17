2a6f3a8bbdedf276639289d478f72b4d1ef885ad
Chocobo1 <Chocobo1@users.noreply.github.com>
2022-11-17 21:29:10 +0800

    Take processing time into account

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 fn main() -> std::process::ExitCode {
+    let start_time = std::time::Instant::now(); // hopefully this operation isn't time-consuming
+
     // https://www.freebsd.org/cgi/man.cgi?query=sysexits&apropos=0&sektion=0&manpath=FreeBSD+14.0-current&arch=default&format=html
     let usage_error_exit_code = std::process::ExitCode::from(64);
 
@@ -7,10 +9,16 @@ fn main() -> std::process::ExitCode {
         _ => return usage_error_exit_code,
     };
     let sleep_time = match sleep_time_str.parse::<u64>() {
-        Ok(x) => x,
+        Ok(x) => std::time::Duration::from_micros(x),
         _ => return usage_error_exit_code,
     };
-    std::thread::sleep(std::time::Duration::from_micros(sleep_time));
+
+    const SLEEP_OVERHEAD: std::time::Duration = std::time::Duration::from_nanos(100);
+    let processing_time = std::time::Instant::now() - start_time + SLEEP_OVERHEAD;
+
+    if sleep_time > processing_time {
+        std::thread::sleep(sleep_time - processing_time);
+    }
 
     std::process::ExitCode::SUCCESS
 }
